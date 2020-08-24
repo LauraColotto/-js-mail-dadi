f2ae68322c3da8883ae9b581183cb5ca666d6045
Lau <arinel88@gmail.com>
2020-08-24 18:00:10 +0200

    Finito esercizio mail

---
+++ mail/js/script.js
@@ -0,0 +1,21 @@
+// 1. Chiedo all'utente la mail
+
+var mailUtente = prompt("Benvenuto! Inserisci la tua mail");
+
+// 2. Confronto il dato con l'array delle mail
+
+var listaMail = ["bea.giuggi@gmail.com", "a.bianchi@gmail.com", "filippo.presti@libero.it", "lauracolotto88@gmail.com"];
+
+var mailTrovata = false;
+
+for (var i = 0; i < listaMail.length; i++) {
+  if(mailUtente == listaMail[i]){
+    mailTrovata = true;
+  }
+}
+
+if(mailTrovata == true){
+  console.log("Accesso consentito");
+} else if(mailTrovata == false){
+  console.log("Accesso negato");
+}
